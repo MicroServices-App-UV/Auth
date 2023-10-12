708ccdae86c3f9b7f5cfafb3577761ad29d50921
rwxp <sdrivert@hotmail.com>
2023-10-11 21:49:13 -0500

    Node_modules removed

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router()
 // @ desc Login/Landing page 
 // @route GET /
 router.get('/', (req, res)=>{
-    res.send('login')
+    res.send('Login')
 })
 
 // @ desc Register
 // @route GET /register
 router.get('/register', (req, res)=>{
-    res.send('register')
+    res.send('Register')
 })
 
 module.exports = router
